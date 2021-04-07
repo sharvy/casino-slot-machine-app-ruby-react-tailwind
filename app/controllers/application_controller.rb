3ada16b94d1bcc948fe31a6a808dac4ee8f4095b
Sharvy Ahmed <sharvy2008@gmail.com>
2021-04-08 03:32:12 +0600

    Moved session handler to it's own place.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
-require "securerandom"
-
-class ApplicationController < ActionController::Base 
-  def current_user
-    session[:current_user_id] ||= SecureRandom.uuid
-    session[:credit] ||= 10
-
-    {
-      user_id: session[:current_user_id],
-      credit: session[:credit],
-    }
-  end
-  helper_method :current_user
+class ApplicationController < ActionController::Base
 end
--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,5 @@
 class GameController < ApplicationController
   def index
+    @game_session = GameSession.start!(session)
   end
 end
