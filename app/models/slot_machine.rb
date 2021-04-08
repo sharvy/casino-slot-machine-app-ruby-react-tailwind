2f6bfb66ffffe625427a14572c261933029b0ef3
Sharvy Ahmed <sharvy2008@gmail.com>
2021-04-09 05:18:52 +0600

    Added Tests.

--- app/models/slot_machine.rb
+++ app/models/slot_machine.rb
@@ -4,28 +4,29 @@ class SlotMachine
   INITIAL_CREDIT = 10
   PER_ROLL_COST = 1
 
-  attr_reader :credit, :game_symbols
+  attr_reader :credit, :game_symbols, :result
 
   def initialize(credit:, game_symbols:)
     @credit = credit
     @game_symbols = game_symbols
+    @result = []
   end
 
   def roll
     raise NotEnoughCreditError if credit < PER_ROLL_COST
 
-    @result = get_random_symbols
+    result = get_random_symbols
 
-    if is_jackpot?(@result) && should_re_roll?
-      @result = get_random_symbols
+    if is_jackpot? && should_re_roll?
+      result = get_random_symbols
     end
 
-    @result
+    result
   end
 
   def jackpot_credit
-    return 0 unless is_jackpot?(@result)
-    GameSymbol.new(@result.first).value
+    return 0 unless is_jackpot?
+    GameSymbol.new(result.first).value
   end
 
   private
@@ -37,8 +38,8 @@ class SlotMachine
     return 0
   end
 
-  def is_jackpot?(array)
-    array.uniq.size == 1
+  def is_jackpot?
+    result.uniq.size == 1
   end
 
   def should_re_roll?
--- spec/models/slot_machine_spec.rb
+++ spec/models/slot_machine_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe SlotMachine do
   let(:credit) { 10 }
-  let(:valid_symbols) { GameSymbol::SYMBOL_NAMES_AND_VALUES.keys }
+  let(:valid_symbols) { GameSymbol.all }
   let(:slot_machine) {
     SlotMachine.new(
       credit: credit,
@@ -27,6 +27,16 @@ RSpec.describe SlotMachine do
     it "returns valid symbols" do
       is_valid_array = @response.all? { |element| valid_symbols.include? element }
       expect(is_valid_array).to be true
+    end
+  end
+
+  describe "#jackpot_credit" do
+    it "checks result and returns jackpot value" do
+      symbol = valid_symbols[0]
+      expected_value = GameSymbol.new(symbol).value
+
+      allow(slot_machine).to receive(:result).and_return([symbol, symbol, symbol])
+      expect(slot_machine.jackpot_credit).to eq(expected_value)
     end
   end
 end
